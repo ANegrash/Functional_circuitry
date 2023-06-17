19ba17ea5e71b9dfd1daac72a6af0f9fc6464d3f
Andrey Negrash <andi.ne@yandex.ru>
2023-06-17 19:06:14 +0300

    Update function_test.v
    Add all pairs test

--- lab2/function_test.v
+++ lab2/function_test.v
@@ -7,6 +7,7 @@ module fun_test;
     wire [11:0] out;
     wire out_busy;
     reg [3:0] count;
+    integer i, j, expected, count_failed;
 
     fun ab_inst(
         .clk_i(clk_reg),
@@ -31,92 +32,29 @@ module fun_test;
         count = 'd0;
         #1
         in_rst = 'd0;
-        forever
-            #1
-            if (~out_busy && ~start && count == 0) begin//8(0x8)
-                in_a = 'd1;
-                in_b = 'd64;
+        count_failed = 0;
+        
+        for (i = 0; i <= 255; i = i+1) begin
+            for (j = 0; j <= 255; j = j+1) begin
+                in_a = i;
+                in_b = j;
                 start = 1;
-                #1
+                @(negedge out_busy);
                 start = 0;
-                count = count + 1;
-             end else if (~out_busy && ~start && count == 1) begin//10(0xa)
-                in_a = 'd2;
-                in_b = 'd25;
-                start = 1;
-                #2
-                start = 0;
-                count = count + 1;
-             end else if (~out_busy && ~start && count == 2) begin//6(0x6)
-                in_a = 'd3;
-                in_b = 'd4;
-                start = 1;
-                #2
-                start = 0;
-                count = count + 1;
-             end else if (~out_busy && ~start && count == 3) begin//0(0x0)
-                in_a = 'd3;
-                in_b = 'd0;
-                start = 1;
-                #2
-                start = 0;
-                count = count + 1;
-             end else if (~out_busy && ~start && count == 4) begin//5(0x5)
-                in_a = 'd5;
-                in_b = 'd1;
-                start = 1;
-                #2
-                start = 0;
-                count = count + 1;
-             end else if (~out_busy && ~start && count == 5) begin//66(0x42)
-                in_a = 'd6;
-                in_b = 'd121;
-                start = 1;
-                #2
-                start = 0;
-                count = count + 1;
-             end else if (~out_busy && ~start && count == 6) begin//800(0x320)
-                in_a = 'd100;
-                in_b = 'd65;
-                start = 1;
-                #2
-                start = 0;
-                count = count + 1;
-             end else if (~out_busy && ~start && count == 7) begin//700(0x2bc)
-                in_a = 'd100;
-                in_b = 'd63;
-                start = 1;
-                #2
-                start = 0;
-                count = count + 1;
-             end else if (~out_busy && ~start && count == 8) begin//30(0x1e)
-                in_a = 'd10;
-                in_b = 'd9;
-                start = 1;
-                #2
-                start = 0;
-                count = count + 1;
-             end else if (~out_busy && ~start && count == 9) begin//3(0x3)
-                in_a = 'd1;
-                in_b = 'd10;
-                start = 1;
-                #2
-                start = 0;
-                count = count + 1;
-             end else if (~out_busy && ~start && count == 10) begin//0(0x0)
-                in_a = 'd0;
-                in_b = 'd0;
-                start = 1;
-                #2
-                start = 0;
-                count = count + 1;
-            end else if (~out_busy && ~start && count == 11) begin//3825(0xef1)
-                in_a = 'd255;
-                in_b = 'd255;
-                start = 1;
-                #2
-                start = 0;
-                count = count + 1;
+                expected = i * $floor($sqrt(j));
+                if (expected == out) begin
+                    $display("Correct! a= %d, b=%d, y=%d", in_a, in_b, out);
+                end else begin
+                    $display("Wrong((( a= %d, b=%d, y=%d", in_a, in_b, out);
+                    count_failed = count_failed + 1;
+                end
             end
+        end
+        
+        if (count_failed == 0) begin
+            $display("All tests passed successfully!");
+        end else begin
+            $display("Testing is not completed. Find %d failed test(s).", count_failed);
+        end
     end
 endmodule
